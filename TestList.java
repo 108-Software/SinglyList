fc395a4c36ff04c63d71c7099b96dcd7be061d50
108-Software <75811746+108-Software@users.noreply.github.com>
2022-11-18 16:45:58 +0300

    Update TestList.java

--- TestList.java
+++ TestList.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
-
 public class TestList {
     public static void main(String[] arg) {
 
-        System.out.print(" 1. Добавить элемент \n 2. Вывести список \n");
-        System.out.print(" 3. Добавить элеммент в начало списка \n 4. Добавить элемент в конец списка \n");
-        System.out.print(" 5. Удаление элемента из начала списка \n 6. Удаление элемента из конца списка \n");
-        System.out.print(" 7. Удаление элемента по значению \n 8. Получение числа элементов в списке \n");
-        System.out.print(" 9. Очистка списка \n");
+        System.out.print(" 1. Добавить элемент в начало списка \n 2. Вывести список \n");
+        System.out.print(" 3. Добавить элемент в конец списка \n 4. Удаление элемента из начала списка \n");
+        System.out.print(" 5. Удаление элемента из конца списка \n 6. Удаление элемента по значению \n");
+        System.out.print(" 7. Получение числа элементов в списке \n 8. Очистка списка \n");
 
         Scanner sc = new Scanner(System.in);
 
         int answer = 0;
+
+        MyList SinglyList = new MyList();
 
         while (answer > 1 | answer < 10) {
             answer = sc.nextInt();
             if (answer==10 | answer>10 | answer == 0 | answer<0){break;}
 
             else {
-                MyList SinglyList = new MyList();
 
                 switch (answer) {
 
@@ -32,9 +31,13 @@ public class TestList {
                     case 2:
                         System.out.print("Значения списка: ");
                         SinglyList.print();
+                        System.out.println();
                         break;
 
                     case 3:
+                        System.out.print("Введите число которое хотите добавить: ");
+                        int addl = sc.nextInt();
+                        SinglyList.ADDLast(addl);
                         break;
 
                     case 4:
@@ -50,14 +53,13 @@ public class TestList {
                         break;
 
                     case 7:
+                        System.out.print("Всего элементов: ");
+                        SinglyList.SUMMNode();
                         break;
 
                     case 8:
+                        SinglyList.REMAll();
                         break;
-
-                    case 9:
-                        break;
-
 
                 }
 
